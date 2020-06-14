7ea2285e7b643c17627533eb922447720cc328e0
libin152@outlook.com <laryli@tencent.com>
2020-06-14 20:35:15 +0800

    init

---
+++ helper.cpp
@@ -0,0 +1,10 @@
+//#include ""
+//
+//#include <cstdlib>
+//bool canCompile() {
+//
+//    int i, ret = std::system("./b.sh 2>&1 > /dev/null");
+//    i = WEXITSTATUS(ret);
+//    printf("My val= %d\n", i);
+//    return false;
+//}
